77610b530f032533526221451f17db4881de3d72
Kyleosimp <KOchate@wil.yearup.org>
2022-07-07 11:21:48 -0400

    Added John's Navbar, deleted redundant and unused imports

--- bankapi_frontend/src/Pages/Bills/CreateBill.js
+++ bankapi_frontend/src/Pages/Bills/CreateBill.js
@@ -4,8 +4,7 @@ import Axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import ReactDatePicker from 'react-datepicker';
-import Select from 'react-select';
+import { NavigationBar } from '../Components/NavigationBar';
 
 
 function CreateBill(){
@@ -52,40 +51,6 @@ function CreateBill(){
         account_id: params.id
     })
 
-    const dateOptions=[
-        {value: "1", label: "1"},
-        {value: "2", label: "2"},
-        {value: "3", label: "3"},
-        {value: "4", label: "4"},
-        {value: "5", label: "5"},
-        {value: "6", label: "6"},
-        {value: "7", label: "7"},
-        {value: "8", label: "8"},
-        {value: "9", label: "9"},
-        {value: "10", label: "10"},
-        {value: "11", label: "11"},
-        {value: "12", label: "12"},
-        {value: "13", label: "13"},
-        {value: "14", label: "14"},
-        {value: "15", label: "15"},
-        {value: "16", label: "16"},
-        {value: "17", label: "17"},
-        {value: "18", label: "18"},
-        {value: "19", label: "19"},
-        {value: "20", label: "20"},
-        {value: "21", label: "21"},
-        {value: "22", label: "22"},
-        {value: "23", label: "23"},
-        {value: "24", label: "24"},
-        {value: "25", label: "25"},
-        {value: "26", label: "26"},
-        {value: "27", label: "27"},
-        {value: "28", label: "28"},
-        {value: "29", label: "29"},
-        {value: "30", label: "30"},
-        {value: "31", label: "31"}
-    ]
-
     function handle(e){{
         const newdata = { ...data};
         newdata[e.target.id] = e.target.value;
@@ -113,6 +78,7 @@ function CreateBill(){
     
     return (
         <div className="container">
+            <NavigationBar />
             <br />
             <div className="row">
                 <div className="">
